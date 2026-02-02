90fafa8ee42c115829fa8f37bfa1daa5e9d6a4b8
kostya fokin <fokin@fanz.ee>
2026-02-02 10:42:28 +0700

    Sorting by Borrow APY should be in ascending

--- pages/borrow/index.vue
+++ pages/borrow/index.vue
@@ -82,7 +82,7 @@ const sortedBorrowList = computed(() => {
       })
     case 'Borrow APY':
       return [...filteredBorrowList.value].sort((a: AnyBorrowVaultPair, b: AnyBorrowVaultPair) => {
-        return Number(b.borrow.interestRateInfo.borrowAPY) - Number(a.borrow.interestRateInfo.borrowAPY)
+        return Number(a.borrow.interestRateInfo.borrowAPY) - Number(b.borrow.interestRateInfo.borrowAPY)
       })
     default:
       return filteredBorrowList.value
